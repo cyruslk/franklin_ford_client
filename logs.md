3d723910e7faf375814d464f666cdb44d0f7fbc4
cyrus <cyrus7@hotmail.fr>
2019-10-24 10:14:53 -0400

    initial commit

---
+++ logs.md
@@ -0,0 +1,5 @@
+### 2019.10.24
+
+For now, the calls to the db are up and running.
+The raw data is stored in the state. {}
+Now need to work with it.
---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      rawData: null,
+      tweetsStrings: null
+    }
+  }
+
+  componentDidMount(){
+    axios.get("https://webhooks.mongodb-stitch.com/api/client/v2.0/app/ford-bot-read-donpl/service/http/incoming_webhook/webhook-ford")
+    .then((response) => {
+      this.setState({
+        rawData: response.data,
+      })
+      
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+  }
+
+
+render(){
+    return (
+      <div className="App">
+          {this.returnTwitterData()}
+      </div>
+    );
+  }
+}
+export default App;
+
