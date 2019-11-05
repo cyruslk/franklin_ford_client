532c48add8b68eb01c8662b903c31097886006b1
cyrus <cyrus7@hotmail.fr>
2019-11-04 19:30:56 -0500

    UI work

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
+import './App.css';
 import axios from "axios";
+const apiLink = "https://webhooks.mongodb-stitch.com/api/client/v2.0/app/ford-bot-read-donpl/service/http/incoming_webhook/webhook-ford";
 
 class App extends Component {
   constructor() {
@@ -11,12 +13,12 @@ class App extends Component {
   }
 
   componentDidMount(){
-    axios.get("https://webhooks.mongodb-stitch.com/api/client/v2.0/app/ford-bot-read-donpl/service/http/incoming_webhook/webhook-ford")
+    axios.get(apiLink)
     .then((response) => {
       this.setState({
         rawData: response.data,
       })
-      
+
     })
     .catch((err) => {
       console.log(err);
@@ -25,11 +27,28 @@ class App extends Component {
 
 
 render(){
+    console.log(this.state.rawData, "||");
     return (
       <div className="App">
+        <header>
+          <h1>In this case, the primacy is with the news organization per se.</h1>
+        </header>
+        <nav>
+          <ul>
+            <li>About</li>
+            <li>Sources</li>
+            <li>Who</li>
+            <li>News</li>
+            <li>Acknowledgments</li>
+          </ul>
+        </nav>
+        <section>
+          <div>
+            @franklinfordbot is a bot that tweets excerpts from the writings of Franklin Ford. An intriguing figure in the history of American journalism, Franklin Ford (1849-1918) is mostly known for his association with John Dewey, with whom he tried to launch Thought News, a "philosophical newspaper" that never saw the light of day. But beyond that footnote in Dewey's career and in journalism history, Ford's role and his contribution need to be revisited, not only because he was a fascinating media theorist and a compelling writer, but also because reading Ford is a jumping-off point for experimentations and theoretical developments that speak to contemporary media problems. In that regard, @franklinfordbot is an experiment, a remediation of Franklin Ford.
+          </div>
+        </section>
       </div>
     );
   }
 }
 export default App;
-
