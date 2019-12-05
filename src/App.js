6dde40df4f9eb88ae7f816769920250e9820b312
cyrus <cyrus7@hotmail.fr>
2019-12-05 13:40:47 -0500

    UI

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from './Home';
 import HashLinkPage from './HashLinkPage';
+var config = require('./config.js');
 
-function App() {
-  return (
-    <div>
-      <Switch>
-        <Route component={HashLinkPage} />
-      </Switch>
-    </div>
-  );
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render(){
+    return (
+      <div>
+        <Switch>
+          <Route component={HashLinkPage} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
