32085aa5fd110733accfb7c31fb1c88129bdc116
cyrus <cyrus7@hotmail.fr>
2019-12-07 15:09:03 -0500

    Typed one cell, now row

--- src/FirstFold.js
+++ src/FirstFold.js
@@ -8,106 +8,62 @@ class FirstFold extends React.Component {
   constructor(props) {
    super(props);
    this.state = {
+     tweets: null,
+     selectedTweets: null,
      displayedTweets: null,
      displayedMetadata: null,
-     stringTypedArray: [],
-     stringTypedIndex: 0
+     tweetStringTypedArray: [],
+     tweetStringTypedIndex: 0,
+     tweetsArray: [],
+     tweetsIndex: 0
    };
  }
 
  componentDidMount(){
-   return this.typingEffect("hello world")
+   if(!this.props.mockDataTweets){
+     return "loading"
+   }else{
+     this.setState({
+       tweets: this.props.mockDataTweets
+     }, () => {
+       return this.initiateTypingEffect(
+         this.state.tweets[this.state.tweetsIndex].randomString
+       )
+     })
+   }
  }
 
-
-  displayTweets = () => {
-  if(!this.props.mockDataTweets){
-    return null
-  }else{
-    let getRandomTweetsStrings = () => {
-        return this.randomlySelectElements(10, this.state.mockDataTweets)
-        .map((ele, index) => {
-          return ele.randomString
-      })
-    }
-
-    return (
-      <div>
-        <span>I find that the sinking fund of New York city dates from 1813, the year following the inception of the funded debt proper.</span>
-      </div>
-    )
-  }
-}
-
-typingEffect = (string) => {
+initiateTypingEffect = (string) => {
   let splitedString = string.split("");
   let splitedStringLength = splitedString.length;
 
   setInterval(() => {
-      return this.pushToTheContainer(splitedString, splitedStringLength)
-    }, 1000);
-}
+      return this.pushToTheStateContainer(splitedString, splitedStringLength)
+    }, 200);
+  }
 
-pushToTheContainer = (array, arrayLength) => {
-  let currentLetter = array[this.state.stringTypedIndex];
 
+pushToTheStateContainer = (array, arrayLength) => {
+  let currentLetter = array[this.state.tweetStringTypedIndex];
   this.setState({
-    stringTypedArray: [...this.state.stringTypedArray, currentLetter]
+    tweetStringTypedArray: [...this.state.tweetStringTypedArray, currentLetter]
   }, () => {
-    if(this.state.stringTypedIndex === arrayLength){
-      this.setState({
-        stringTypedIndex: 0,
-        stringTypedArray: []
-      })
+    if(this.state.tweetStringTypedIndex === arrayLength){
+      return;
     }else{
       this.setState({
-        stringTypedIndex: this.state.stringTypedIndex+1
+        tweetStringTypedIndex: this.state.tweetStringTypedIndex+1
       })
     }
   })
 }
 
 
-displayMetadataFromTweets = () => {
-    if(!this.props.mockDataTweets){
-      return null;
-    }
-    let returnTweetMetadata = () => {
-      return (
-        <div className="metadata_first_fold">
-          <span>Tweet_created_at:  {this.props.mockDataTweets[this.props.typistIndex].twitterData.Tweet_created_at}</span>
-          <span>Tweet_id_string: {this.props.mockDataTweets[this.props.typistIndex].twitterData.Tweet_id_string}</span>
-          <span>Author_name: {this.props.mockDataTweets[this.props.typistIndex].sourceData.Author_name}</span>
-          <span>PDF_name: {this.props.mockDataTweets[this.props.typistIndex].sourceData.PDF_name}</span>
-        </div>
-      )
-    }
-
-    return (
-      <div>
-          {returnTweetMetadata()}
-      </div>
-    )
-}
-
-
-next = () => {
-  this.setState({
-    typistIndex: this.state.typistIndex+1
-  }, () => {
-    if(this.state.typistIndex === this.state.mockDataTweets.length-1){
-      this.setState({
-        typistIndex: 0
-      })
-    }
-  })
-}
-
-renderStateOnView = () => {
-  if(this.state.stringTypedArray.length === 0){
+renderStringTyped = () => {
+  if(this.state.tweetStringTypedArray.length === 0){
     return null
   }
-  let stateMaped = this.state.stringTypedArray
+  let stateMaped = this.state.tweetStringTypedArray
   .map((ele, index) => {
     return (
       <span key={index}>
@@ -115,17 +71,20 @@ renderStateOnView = () => {
       </span>
     )
   })
-  return stateMaped;
+  return (
+    <div>
+      {stateMaped}
+    </div>
+  )
 }
 
   render(){
     return (
       <main className="first_fold_container">
           <div className="typed_tweets">
-            {this.displayTweets()}
+            {this.renderStringTyped()}
           </div>
           <div className="displayed_metadata">
-          {this.displayMetadataFromTweets()}
         </div>
       </main>
     );
