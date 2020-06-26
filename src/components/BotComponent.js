8810b042c52fa572757be4a3742061f87e84eca5
cyrus <cyrus7@hotmail.fr>
2020-06-26 14:21:16 -0400

    Feat: Ouput, Refactor: Chatbot architecture

--- src/components/BotComponent.js
+++ src/components/BotComponent.js
@@ -1,92 +1,49 @@
 import React from 'react';
-import ReactInterval from 'react-interval';
-import { HashLink as Link } from 'react-router-hash-link';
-import BotComponentSpan from "./BotComponentSpan";
-import BotComponentBody from "./BotComponentBody";
+import BotComponentInput from "./BotComponentInput";
 import App from "../App.css";
 
 
 class BotComponent extends React.Component {
 
-  ws = new WebSocket('ws://35.226.112.179:8880/generator')
-
-
   constructor(props) {
     super(props);
     this.state = {
-      userInput: "",
-      dataFromServer: null
+      displayPrediction: null
     };
   };
 
-  componentDidMount(){
-    this.ws.onopen = () => {
-      console.log('connected')
-    };
-
-    this.ws.onmessage = evt => {
-      this.setState({
-        dataFromServer: evt.data
-      })
-    };
-  };
-
-  componentDidUpdate(prevState, actualState){
-    if(prevState.dataFromServer !== actualState.dataFromServer){
-      console.log(this.state);
-    }
-  }
-
-
-  handleChange = (event) => {
+  displayPrediction = (data) => {
+    let displayPrediction = data;
     this.setState({
-      userInput: event.target.value
+      displayPrediction
     })
-  }
+  };
 
-  sendDataToServer = () => {
-    let dataToSend = this.state.userInput;
-    console.log(`${dataToSend} sent to server`);
-    this.ws.send(dataToSend);
-  }
-
-   renderInputQuestion = () => {
-     return (
-       <div className="bot_input_question_section">
-
-          <input
-            value={this.state.userInput}
-            onChange={this.handleChange}
-            placeholder={"here"}/>
-
-            <button onClick={this.sendDataToServer}>
-              send here
-            </button>
-
-       </div>
-     );
-   };
-
-
-
-   renderClosingDiv = () => {
-     return (
-       <div>
-          <span onClick={this.props.closeChatBot}>X</span>
-       </div>
-     )
-   }
-
-
-  render() {
-
+  renderBotPrediction = () => {
+    if(!this.state.displayPrediction){
+      return "loading"
+    }
     return (
-      <div className="bot_component_container">
-        {this.renderClosingDiv()}
-        {this.renderInputQuestion()}
-        <BotComponentBody {...this.state} />
+      <div style={{position: "fixed", top: "2vw", backgroundColor: "red"}}>{this.state.displayPrediction}</div>
+    )
+  };
+
+  renderBotInputComponent = () => {
+    return (
+      <BotComponentInput
+        closeChatBot={this.props.closeChatBot}
+        displayPrediction={this.displayPrediction}
+      />
+    )
+  }
+
+  render(){
+    return(
+      <div>
+        {this.renderBotInputComponent()}
+        {this.renderBotPrediction()}
       </div>
-    );
+    )
   }
 };
 
---
+++ src/components/BotComponentInput.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import ReactInterval from 'react-interval';
+import { HashLink as Link } from 'react-router-hash-link';
+import App from "../App.css";
+
+
+class BotComponentInput extends React.Component {
+
+  ws = new WebSocket('ws://35.226.112.179:8880/generator')
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      userInput: "",
+      dataFromServer: null
+    };
+  };
+
+  componentDidMount(){
+    this.ws.onopen = () => {
+      console.log('connected')
+    };
+
+    this.ws.onmessage = evt => {
+      this.setState({
+        dataFromServer: evt.data
+      }, () => {
+        this.props.displayPrediction(evt.data)
+      })
+    };
+  };
+
+
+  handleChange = (event) => {
+    this.setState({
+      userInput: event.target.value
+    })
+  }
+
+  sendDataToServer = () => {
+    let dataToSend = this.state.userInput;
+    console.log(`${dataToSend} sent to server`);
+    this.ws.send(dataToSend);
+  }
+
+   renderInputQuestion = () => {
+     return (
+       <div className="bot_input_question_section">
+
+          <input
+            value={this.state.userInput}
+            onChange={this.handleChange}
+            placeholder={"here"}/>
+
+            <button onClick={this.sendDataToServer}>
+              send here
+            </button>
+
+       </div>
+     );
+   };
+
+
+
+   handleCloseChatBot = () => {
+     return (
+       <div>
+          <span onClick={this.props.closeChatBot}>X</span>
+       </div>
+     )
+   }
+
+
+  render() {
+
+    return (
+      <div className="bot_component_container">
+        {this.handleCloseChatBot()}
+        {this.renderInputQuestion()}
+      </div>
+    );
+  }
+};
+
+export default BotComponentInput;
